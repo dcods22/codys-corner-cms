18bf74876ca3e4fff0d76a9225de6d658a37c54d
Dcods22 <Daniel.cody2@marist.edu>
2018-09-19 21:59:32 -0400

    Updated apis

--- src/api/CCApi.js
+++ src/api/CCApi.js
@@ -4,17 +4,17 @@ const BASE_URL = process.env.REACT_APP_BASE_URL + "/cc-services/api";
 class CCApi {
   getArticle = articleId => {
     const URL = BASE_URL + "/articles/" + articleId;
-    return axios.get(URL).then(response => response.data);
+    return axios.get(URL).then(response => response.data.entity);
   };
 
   getAllTags = () => {
     const URL = BASE_URL + "/tags";
-    return axios.get(URL).then(response => response.data);
+    return axios.get(URL).then(response => response.data.entity);
   };
 
   getArticleStatuses = () => {
     const URL = BASE_URL + "/articles/statuses";
-    return axios.get(URL).then(response => response.data);
+    return axios.get(URL).then(response => response.data.entity);
   };
 }
 
--- src/api/CMSApi.js
+++ src/api/CMSApi.js
@@ -4,17 +4,17 @@ const BASE_URL = process.env.REACT_APP_BASE_URL + "/cms/api";
 class CMSApi {
   getMostRecentArticles() {
     const url = BASE_URL + "/articles/mostRecent";
-    return axios.get(url).then(response => response.data);
+    return axios.get(url).then(response => response.data.entity);
   }
 
   getNotPublishedArticles() {
     const url = BASE_URL + "/articles/notPublished";
-    return axios.get(url).then(response => response.data);
+    return axios.get(url).then(response => response.data.entity);
   }
 
   saveArticle(article) {
     const url = BASE_URL + "/articles";
-    return axios.post(url, article).then(response => response.data);
+    return axios.post(url, article).then(response => response.data.entity);
   }
 }
 
